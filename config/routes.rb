7c892639af2279f2d0e7c2b9d58e903bdda54616
higgs <matt@secureddesigns.net>
2025-09-13 09:17:54 -0400

    adding a user sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "static_pages#home"
+
   get "/signup",   to: "users#new"
   get "/help",     to: "static_pages#help"
   get "/about",    to: "static_pages#about"
   get "/contact",  to: "static_pages#contact"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :users
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  root "static_pages#home"
+  
 end
